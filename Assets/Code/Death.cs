fdbdd110cd2dfef31c237df961c7515f4c9916bc
cdejtiar <cdejtiar14@gmail.com>
2023-09-28 13:10:32 -0300

    testing trigger on damagers

--- Assets/Code/Death.cs
+++ Assets/Code/Death.cs
@@ -1,12 +1,18 @@
+using System.Security;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Death : MonoBehaviour
 {
+    [SerializeField] private GameObject gameObjectPrefab;
     private void OnCollisionEnter2D(Collision2D other)
     {
         var health = other.gameObject.GetComponent<Health>();
+        var startingHealth = health.Current;
         health.Damage(25);
+
+        var trigger = gameObjectPrefab.GetComponent<Collider2D>();
+        trigger.isTrigger = (health.Current != startingHealth) ? true : false; // "funciona"
     }
 }
