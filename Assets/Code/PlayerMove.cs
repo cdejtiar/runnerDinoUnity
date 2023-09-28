782d23f4c3a594946af1c91fac126eed132cf459
cdejtiar <cdejtiar14@gmail.com>
2023-09-27 21:53:54 -0300

    changes in playermovement, now it JUMPS BETTER

--- Assets/Code/PlayerMove.cs
+++ Assets/Code/PlayerMove.cs
@@ -7,7 +7,9 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] private float speed = 4;
     [SerializeField] private Rigidbody2D rb; //Declaro
     [SerializeField] private SpriteRenderer spriteRenderer;
-    [SerializeField] public float jumpForce;
+    [SerializeField] public float jumpForce = 100;
+    [SerializeField] public float gravityScale = 5;
+    [SerializeField] public float fallGravityScale = 10;
 
 
     void Update()
@@ -16,8 +18,21 @@ public class PlayerMove : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            rb.velocity += new Vector2(0, jumpForce); //SALTA!!!!! Falta que sea más natural.
+            rb.velocity += new Vector2(1, jumpForce) * speed; //SALTA!!!!! Falta que sea más natural.
+            /*if (Mathf.Abs(rb.velocity.y) < 0.01f) // Solo permitir saltos cuando la velocidad vertical sea baja (en el suelo)
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }*/
         }
+
+        /*if (rb.velocity.y > 0)
+        {
+            rb.gravityScale = gravityScale;
+        }
+        else
+        {
+            rb.gravityScale = fallGravityScale;
+        }/*
     }
     /*void OnMove(InputValue value)
     {
@@ -29,6 +44,6 @@ public class PlayerMove : MonoBehaviour
         if (direction.x != 0)
         {
             spriteRenderer.flipX = direction.x < 0;
-        }
-    }*/
+        }*/
+    }
 }
