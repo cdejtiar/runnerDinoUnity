ae8dfbecc762349bba4e11ff6bdff1a248952be1
MaxiCo1 <coletti.maxi@gmail.com>
2023-11-02 16:15:49 -0300

    distance working

--- Assets/Code/PowerUp.cs
+++ Assets/Code/PowerUp.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PowerUp : MonoBehaviour
 {
-    private int cantidadARecuperar = 20; // Cantidad de salud que se recupera al recoger el power-up
+    private int cantidadARecuperar = 50; // Cantidad de salud que se recupera al recoger el power-up
 
     [SerializeField] private PlayerMove player;
     void OnTriggerEnter2D(Collider2D other)
